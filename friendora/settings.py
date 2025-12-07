8b306dc65a5fb389c17bb621274f9c74b7953350
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-07 16:48:02 +0200

    feat(accounts): add TempExample model and initial migration

--- friendora/settings.py
+++ friendora/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "frontend" / "dist"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
