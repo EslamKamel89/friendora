6366e076b81f03fb967ae8248693d76b323c418e
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-11 08:09:18 +0200

    feat(auth): add JWT-based registration and login endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,10 @@
 from django.urls import URLPattern, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns: list[URLPattern] = []
+from . import views
+
+urlpatterns: list[URLPattern] = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from accounts.models import User
+from accounts.serializers import RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
