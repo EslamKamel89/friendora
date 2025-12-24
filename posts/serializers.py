ee9aa7155cde240e609f5daccd7053bc8ca166fd
EslamKamel89 <elsamkamelforex@gmail.com>
2025-12-24 16:34:39 +0200

    feat(posts): support tag assignment via serializer with slug output

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,8 @@
+from typing import Required
+
 from rest_framework import serializers
 
-from posts.models import Post
+from posts.models import Post, Tag
 
 MAX_IMAGE_SIZE = 5 * 1024 * 1024  # 5 MB
 ALLOWED_CONTENT_TYPES = ("image/jpeg", "image/png", "image/webp")
@@ -8,9 +10,39 @@ ALLOWED_CONTENT_TYPES = ("image/jpeg", "image/png", "image/webp")
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
-    image = serializers.ImageField(
-        required=False, allow_null=True, allow_empty_file=True
+    tags = serializers.SlugRelatedField(
+        read_only=True,
+        many=True,
+        slug_field="name",
     )
+    tags_input = serializers.ListField(
+        child=serializers.CharField(),
+        write_only=True,
+        required=True,
+    )
+    image = serializers.ImageField(
+        required=False,
+        allow_null=True,
+        allow_empty_file=True,
+    )
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop("tags_input", [])
+        post = Post.objects.create(**validated_data)
+        for tag_name in tags_data:
+            tag, _ = Tag.objects.get_or_create(name=tag_name)
+            post.tags.add(tag)
+        return post
+
+    def update(self, instance: Post, validated_data):
+        tags_data = validated_data.pop("tags_input", [])
+        instance = super().update(instance, validated_data)
+        if tags_data is not None:
+            instance.tags.clear()
+            for tag_name in tags_data:
+                tag, _ = Tag.objects.get_or_create(name=tag_name)
+                instance.tags.add(tag)
+        return instance
 
     class Meta:
         model = Post
